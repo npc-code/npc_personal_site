35a4603403790b18428fe9e0673bc4777dabfa93
Nic <nic.ceynowa@gmail.com>
2021-03-15 19:53:38 -0400

    here we go

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+aws s3 sync ../.. s3://dev-site-20210315214923790700000003 --exclude ".git" --exclude ".github" --profile deploymentuser-005240044a15e466
+aws cloudfront create-invalidation --distribution-id E4V6CMQZH9ON --paths '/*' --profile deploymentuser-005240044a15e466
